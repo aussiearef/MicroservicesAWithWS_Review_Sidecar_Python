cd2802336988fe4d48141cfdd60b6b0e814412e3
aussiearef <arefkr@gmail.com>
2023-06-25 10:18:04 +1000

    Improved code

--- HandleReviewEvent_Python.py
+++ HandleReviewEvent_Python.py
@@ -5,26 +5,20 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Booking')
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-        sns_message = json.loads(record['Sns']['Message'])
-        booking_id = sns_message.get('BookingId')
-        status = sns_message.get('Status')
-        if not booking_id:
-            print('SNS Event did not return a valid BookingId')
-            continue
-        try:
-            response = table.update_item(
-                Key={
-                    'Id': booking_id
-                },
-                UpdateExpression='SET #status = :val',
-                ExpressionAttributeNames={
-                    '#status': 'Status'
-                },
-                ExpressionAttributeValues={
-                    ':val': status
-                }
-            )
-            print(f'Booking with ID {booking_id} updated successfully.')
-        except Exception as e:
-            print(f'Error updating booking with ID {booking_id}: {e}')
+    sns_message = json.loads(event['Records'][0]['Sns']['Message'])
+    booking_id = sns_message.get('BookingId')
+    status = sns_message.get('Status')
+    table.update_item(
+        Key={
+            'Id': booking_id
+        },
+        UpdateExpression='SET #status = :val',
+        ExpressionAttributeNames={
+            '#status': 'Status'
+        },
+        ExpressionAttributeValues={
+            ':val': status
+        }
+    )
+    context.log(f"Booking with ID {booking_id} updated successfully.")
+        
